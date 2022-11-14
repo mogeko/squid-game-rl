5a07c3564b54de0b2c832c1115ea5a9a81179100
Mogeko <zhengjunyi@live.com>
2022-11-14 20:12:57 +0100

    feat: control Player's movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private CharacterController controller;
+    private float gravity = 9.8f;
+    private float speed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.controller  = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        float s = this.speed * Time.deltaTime;
+
+        Vector3 terget = (transform.right * v) - (transform.forward * h);
+        this.controller.Move(terget * this.speed * Time.deltaTime);
+        if (!controller.isGrounded){
+            this.controller.Move(Vector3.down * this.gravity * Time.deltaTime);
+        }
     }
 }
