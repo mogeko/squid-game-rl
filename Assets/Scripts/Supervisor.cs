459d3ef5421b57298484a5266437f8c5201d7f4f
Mogeko <zhengjunyi@live.com>
2022-11-15 01:43:07 +0100

    feat: supervisor will check player after a random time

--- Assets/Scripts/Supervisor.cs
+++ Assets/Scripts/Supervisor.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Supervisor : MonoBehaviour
-{
+public class Supervisor : MonoBehaviour {
+    private Player player;
+
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        this.player = GameObject.Find("Player").GetComponent<Player>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (!IsInvoking("checkPlayer")) {
+            var randomTime = Random.Range(4, 7);
+            Invoke("remind", randomTime - 1);
+            Invoke("checkPlayer", randomTime);
+        }
+    }
+
+    void remind() {
+        Debug.Log("We will check you after 1 second!");
+    }
+
+    void checkPlayer() {
+        if (this.player.getIsMoving()) {
+            this.player.dead();
+        }
     }
 }
